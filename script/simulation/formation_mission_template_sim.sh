aa02fefe25736b29a43cd070237c76f9499456c4
lddddd1997 <lddddd1997@gmail.com>
2021-01-06 21:38:46 +0800

    Add state estimator node

--- script/simulation/formation_mission_template_sim.sh
+++ script/simulation/formation_mission_template_sim.sh
@@ -1,22 +1,27 @@
 ##Task start script
-gnome-terminal --window -e 'bash -c "roslaunch px4_application base_uav_formation_sim.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application base_uav_control.launch uav_id:=1; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application app_uav_mission_template.launch uav_id:=1; exec bash"' \
+gnome-terminal --window --geometry=80x24+0+0 -e 'bash -c "roslaunch px4_application base_sim_multi_uav_ground_truth.launch; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application base_uav_control.launch uav_id:=1; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application base_uav_estimator.launch uav_id:=1; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application app_uav_mission_template.launch uav_id:=1; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_display.launch uav_id:=1; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_setting.launch uav_id:=1; exec bash"' \
-gnome-terminal --window -e 'bash -c "sleep 5; roslaunch px4_application base_uav_control.launch uav_id:=2; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application app_uav_mission_template.launch uav_id:=2; exec bash"' \
+gnome-terminal --window --geometry=80x24+1200+0 -e 'bash -c "sleep 6; roslaunch px4_application base_uav_control.launch uav_id:=2; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application base_uav_estimator.launch uav_id:=2; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application app_uav_mission_template.launch uav_id:=2; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_display.launch uav_id:=2; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_setting.launch uav_id:=2; exec bash"' \
-gnome-terminal --window -e 'bash -c "sleep 5; roslaunch px4_application base_uav_control.launch uav_id:=3; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application app_uav_mission_template.launch uav_id:=3; exec bash"' \
+gnome-terminal --window --geometry=80x24+0+700 -e 'bash -c "sleep 6; roslaunch px4_application base_uav_control.launch uav_id:=3; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application base_uav_estimator.launch uav_id:=3; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application app_uav_mission_template.launch uav_id:=3; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_display.launch uav_id:=3; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_setting.launch uav_id:=3; exec bash"' \
-gnome-terminal --window -e 'bash -c "sleep 5; roslaunch px4_application base_uav_control.launch uav_id:=4; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application app_uav_mission_template.launch uav_id:=4; exec bash"' \
+gnome-terminal --window --geometry=80x24+1200+700 -e 'bash -c "sleep 6; roslaunch px4_application base_uav_control.launch uav_id:=4; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application base_uav_estimator.launch uav_id:=4; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application app_uav_mission_template.launch uav_id:=4; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_display.launch uav_id:=4; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_setting.launch uav_id:=4; exec bash"' \
-gnome-terminal --window -e 'bash -c "sleep 5; roslaunch px4_application base_uav_control.launch uav_id:=5; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application app_uav_mission_template.launch uav_id:=5; exec bash"' \
+gnome-terminal --window --geometry=80x24+600+320 -e 'bash -c "sleep 6; roslaunch px4_application base_uav_control.launch uav_id:=5; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application base_uav_estimator.launch uav_id:=5; exec bash"' \
+--tab -e 'bash -c "sleep 6; roslaunch px4_application app_uav_mission_template.launch uav_id:=5; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_display.launch uav_id:=5; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch px4_application base_gcs_setting.launch uav_id:=5; exec bash"' \
